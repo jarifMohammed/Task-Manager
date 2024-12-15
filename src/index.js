8eb223cbc5480254510a6ef3cf270e178e31e9e7
jarifMohammed <mohammedjarif@protonmail.com>
2024-12-16 03:27:20 +0600

    delete endpoint added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
+const task = require('./models/task')
 
 const app = express()
 
@@ -96,6 +97,22 @@ app.patch('/users/:id' ,async (req,res)=>{
     }
 })
 
+app.delete('/users/:id' , async (req,res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user){
+            return res.status(404).send()
+
+        }
+        res.send(user)
+
+    }catch(e) {
+        res.status(500).send()
+
+    }
+})
+
+
 
 
 app.post('/tasks', async (req, res) => {
@@ -151,6 +168,47 @@ app.get('/tasks/:id', async (req, res) => {
     // }).catch((e) => {
     //     res.status(500).send(e)
     // })
+})
+app.patch('/tasks/:id' , async (req,res) =>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates= ['description' , 'completed']
+
+    const isValid = updates.every((update)=>{
+        return allowedUpdates.includes(update)
+
+        
+
+    })
+    if(!isValid){
+        return res.status(400).send({error :'Invalid operation'}) 
+    }
+
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id , req.body , {new:true , runValidators:true})
+
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(404).send(e)
+    }
+
+    
+})
+app.delete('/tasks/:id' , async (req,res)=>{
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            return res.status(404).send()
+
+        }
+        res.send(task)
+
+    }catch(e) {
+        res.status(500).send()
+
+    }
 })
 
 app.listen(port, () => {
